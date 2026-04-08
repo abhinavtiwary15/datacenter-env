0bd3b66d66ef600f03cffae7db75d240778dc038
aaravtrehan <abhinavtiwary498@gmail.com>
2026-04-08 23:42:29 +0530

    fix: openenv validate issues

--- server/app.py
+++ server/app.py
@@ -308,9 +308,3 @@ def schema():
             "done": {"type": "boolean"}
         }
     }
-def main():
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
-
-if __name__ == "__main__":
-    main()
